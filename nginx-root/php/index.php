fb23dbe05321c2477d4857699a43d4b94ddd5e38
ma yingkui <maeikei@gmail.com>
2013-04-06 23:35:35 +0900

    mv to nginx root

--- nginx-root/php/index.php
+++ nginx-root/php/index.php
@@ -1,1 +1,11 @@
-<>
+<?php
+    $var="something";
+    echo "Hello world";
+    echo $var;
+    /****************************************************
+     * Remember that header() must be called before any actual output is sent,
+     * either by normal HTML tags, blank lines in a file, or from PHP.
+     *****************************************************/
+    header("Location: /users/guest");
+    exit;
+?>
