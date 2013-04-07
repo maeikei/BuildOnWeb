c0308992600e504491919bbb3ae5840b7046321e
mayingkui <maeikei@gmail.com>
2013-04-07 08:35:24 +0800

    change directory.
    Signed-off-by: mayingkui <maeikei@gmail.com>

--- nginx-root/var/www/index.php
+++ nginx-root/var/www/index.php
@@ -1,4 +1,10 @@
 <?php
+	$url = "192.168.0.150";
+	$user = "root";
+	$pass = "ma";
+	$db = "BuildOnWeb.db";
+	$link = mysql_connect($url,$user,$pass) or die("Failed! mysql connect");
+	$sdb = mysql_select_db($db,$link) or die("Failed! select db");
     $redirect_url= "Location: http://" . $_SERVER['SERVER_ADDR'] . "/users/guest";
     echo $redirect_url . "\n";
     echo $_SERVER['HTTP_HOST'] . "\n";
