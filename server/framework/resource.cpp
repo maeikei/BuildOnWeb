4edbe229974c14a14438edc975123d1243227794
ma yingkui <maeikei@gmail.com>
2013-04-26 12:38:49 +0900

    add framework of like rest

---
+++ server/framework/resource.cpp
@@ -0,0 +1,15 @@
+//
+// resource.cpp
+// ~~~~~~~~~~
+//
+
+#include "resource.hpp"
+#include <vector>
+#include <thread>
+#include <memory>
+
+namespace http {
+namespace server_threadpool {
+
+} // namespace server_threadpool
+} // namespace http
---
+++ server/framework/resource.hpp
@@ -0,0 +1,33 @@
+//
+// resource.hpp
+// ~~~~~~~~~~
+//
+//
+
+#ifndef HTTP_SERVER3_RESOURCE_HPP
+#define HTTP_SERVER3_RESOURCE_HPP
+
+#include <string>
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+#include <network/uri.hpp>
+
+namespace http {
+namespace server_threadpool {
+
+class resource
+  : private boost::noncopyable
+{
+public:
+    resource(network::uri &u);
+    void get(void);
+    void post(void);
+    void put(void);
+    void remove(void);
+};
+
+} // namespace server_threadpool
+} // namespace http
+
+#endif // HTTP_SERVER3_RESOURCE_HPP
